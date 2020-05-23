e498d71d73a04e3b5eedcb758727d7e6667733ed
ravshanmakhmadaliev <ravshan9779@mail.ru>
2020-05-23 16:30:34 +0500

    first commit

---
+++ src/main/java/uz/kyuar/KyuarProtoApplication.java
@@ -0,0 +1,13 @@
+package uz.kyuar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyuarProtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyuarProtoApplication.class, args);
+	}
+
+}
